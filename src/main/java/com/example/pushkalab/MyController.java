a0a7518e21c080494e869944dcf005959188aa28
YansLeem <yans1996@yandex.ru>
2019-02-13 02:53:52 +0300

    Added testtttt

---
+++ src/main/java/com/example/pushkalab/MyController.java
@@ -0,0 +1,27 @@
+package com.example.pushkalab;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/pushka")
+    public String pushka(@RequestParam(name = "name", required = false, defaultValue = "Army") String name,
+                         Map<String, Object> model) {
+   System.out.println("esdrth");
+        model.put("name", name);
+        return "pushkaH";
+    }
+    @GetMapping
+    public String main (Map<String, Object > model){
+        model.put("some", "Hello, Army!");
+        return "main";
+    }
+
+}
---
+++ src/test/java/com/example/testim.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static javax.swing.UIManager.get;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//k
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class testim {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGreeting() throws Exception{
+
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("<html>\n" +
+                        "<body>\n" +
+                        "Hello, Army!\n" +
+                        "</body>\n" +
+                        "</html>")));
+
+        this.mockMvc.perform(get("/pushka?name=lol")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("<html>\n<body>\n<div> Hello, lol</div>\n</body>\n</html>")));
+    }
+}
