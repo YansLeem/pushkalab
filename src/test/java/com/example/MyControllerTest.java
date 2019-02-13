bfcb477a87e1d3ef046e040ac7b6169fcb368902
YansLeem <yans1996@yandex.ru>
2019-02-13 03:22:09 +0300

    Added travis.yml v2

--- src/test/java/com/example/MyControllerTest.java
+++ src/test/java/com/example/MyControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 
 import static javax.swing.UIManager.get;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -18,21 +19,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class testim {
+public class MyControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void testGreeting() throws Exception{
 
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform((RequestBuilder) get("/")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("<html>\n" +
                         "<body>\n" +
                         "Hello, Army!\n" +
                         "</body>\n" +
                         "</html>")));
 
-        this.mockMvc.perform(get("/pushka?name=lol")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform((RequestBuilder) get("/pushka?name=lol")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("<html>\n<body>\n<div> Hello, lol</div>\n</body>\n</html>")));
     }
 }
